81f206a71db0254f7670f1111b3e7cf6be9b6f7b
neldnermaksim <neldnermaksim8@gmail.com>
2025-10-31 19:46:02 +0500

    Task7

---
+++ Tyuiu.NeldnerMK.Sprint3.Task7.V16.Test/DataServiceTest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.NeldnerMK.Sprint3.Task7.V16.Lib;
+
+namespace Tyuiu.NeldnerMK.Sprint3.Task7.V16.Test
+{
+    [TestClass]
+
+    public class DataServiseTest
+    {
+        [TestMethod]
+        public void ValidGetCompare()
--- Tyuiu.NeldnerMK.Sprint3.Task7.V16/Program.cs
+++ Tyuiu.NeldnerMK.Sprint3.Task7.V16/Program.cs
@@ -2,10 +2,10 @@
 using System.Text;
 using System.Threading.Tasks;
 
-using Tyuiu.NeldnerMK.Sprint3.Task6.V14.Lib;
+using Tyuiu.NeldnerMK.Sprint3.Task7.V16.Lib;
 
 
-namespace Tyuiu.NeldnerMK.Sprint3.Task6.V14
+namespace Tyuiu.NeldnerMK.Sprint3.Task7.V16
 {
     class Program
     {
@@ -14,29 +14,46 @@ namespace Tyuiu.NeldnerMK.Sprint3.Task6.V14
             Console.Title = "Спринт №1 | Выполнил: Нельднер М. К . | СМАРТб-25-1";
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* Спринт  #3                                                               *");
-            Console.WriteLine("* Задание #6                                                            *");
-            Console.WriteLine("* Вариант #14                                                          *");
+            Console.WriteLine("* Задание #7                                                           *");
+            Console.WriteLine("* Вариант #16                                                          *");
             Console.WriteLine("* Выполнил: Нельднер Максим Константинович | СМАРТб-25-1                   *");
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                 *");
-            Console.WriteLine("*Напишите программу, которая ищет среди целых чисел, принадлежащих числовому отрезку [7, 16] количество всех делителей больше 5");
+            Console.WriteLine("* Напишите программу ");
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ: ");
             Console.WriteLine();
 
             DataService ds = new DataService();
 
-      
-            int startValue = 7;
-            int stopValue = 16;
-            
 
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Стоп шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
 
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
             Console.WriteLine("****************************************************************************");
 
-            Console.WriteLine(ds.GetSumTheDivisors(startValue, stopValue));
+            Console.WriteLine("+----------+----------+");
+            Console.WriteLine("|    x     |   f(x)   |");
+            Console.WriteLine("+----------+----------+");
+            for (int i = 0; i < len; i++)
+            {
+                
+                Console.WriteLine("|{0,5:d}     | {1,7:f2}  |", startValue + i, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+----------+----------+");
         }
     }
 }
