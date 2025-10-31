9b61cd41ed4b1de2ae0dd8b9827ca01a0391c47f
neldnermaksim <neldnermaksim8@gmail.com>
2025-10-31 20:07:57 +0500

    Task7

--- Tyuiu.NeldnerMK.Sprint3.Task7.V16.Test/DataServiceTest.cs
+++ Tyuiu.NeldnerMK.Sprint3.Task7.V16.Test/DataServiceTest.cs
@@ -11,5 +11,33 @@ namespace Tyuiu.NeldnerMK.Sprint3.Task7.V16.Test
         [TestMethod]
         public void ValidGetCompare()
         {
+            DataService ds = new DataService();
 
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = stopValue - startValue + 1;
+
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+
+            valueWaitArray[0] = -36.41;
+            valueWaitArray[1] = -22.07;
+            valueWaitArray[2] = 5.68;
+            valueWaitArray[3] = 16.72;
+            valueWaitArray[4] = 8.35;
+            valueWaitArray[5] = -0.50;
+            valueWaitArray[6] = 9.63;
+            valueWaitArray[7] = 16.29;
+            valueWaitArray[8] = 5.01;
+            valueWaitArray[9] = -22.40;
+            valueWaitArray[10] = -36.30;
+
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(res, valueWaitArray);
         }
+    }
+}
